355d3135380fe87dbb9652efc65f13bc5a0951ea
deim0s <atymick@gmail.com>
2016-05-05 03:54:45 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# DS3-SaveBackupTool
-Automated save file backup created for Dark Souls 3
+# Dark Souls III - Save Backup Tool
+
+Automated save backup tool for Dark Souls III utilizing rolling backups, meaning if set to five, on the sixth save the first save is removed.
+
+## Configuration
+This tool requires the configuration file *config.properties* located in the same directory as the executable, containing the following information:
+```ini
+#_____Dark Souls III Backup Configuration_____
+
+# Location of save file that will be backed up
+save_file=C:\\Users\\<USER_NAME>\\AppData\\Roaming\\DarkSoulsIII\\<STEAM_ID>\\DS30000.sl2
+
+# Location of the directory that will contain the backed up saves
+backup_path=C:\\Users\\<USER_NAME>\\AppData\\Roaming\\DarkSoulsIII\\BACKUPS
+
+# Prefix and extension of backups created
+# If backup_prefix=AAA and backup_extension=zzz then the backup will be AAA_001.zzz
+backup_prefix=DS30000
+backup_extension=sl2
+
+# Maximum number of backups to create before rolling over
+max_saves=10
+
+# Time between backups in minuets
+delay=5
+
+# DO NOT CHANGE
+# Tell the program the last backup that was created
+# If last_backup=6 then AAA_006.zzz was the most recent backup
+last_backup=0
+```
+
+## Running
+```sh
+java -cp DarkSoulsIII.jar save.RollingBackup
+```
---
+++ src/save/RollingBackup.java
@@ -0,0 +1,71 @@
+package save;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class RollingBackup {
+    public static Path source;
+    public static Path destination;
+    public static String prefix;
+    public static String extension;
+    public static int mIteration;
+    public static int cIteration;
+    public static int delay;
+    
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+        File executionPath = new File(RollingBackup.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+        final File configuration = new File(executionPath.getParentFile(), "config.properties");
+        final Properties properties = new Properties();
+        try (InputStream in = new FileInputStream(configuration)) {
+            properties.load(in);
+            source      = Paths.get(properties.getProperty("save_file"));
+            destination = Paths.get(properties.getProperty("backup_path"));
+            prefix     = properties.getProperty("backup_prefix");
+            extension   = properties.getProperty("backup_extension");
+            mIteration  = Integer.parseInt(properties.getProperty("max_saves"));
+            cIteration  = Integer.parseInt(properties.getProperty("last_backup"));
+            delay       = Integer.parseInt(properties.getProperty("delay"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (OutputStream out = new FileOutputStream(configuration)) {
+                properties.setProperty("last_backup", "" + cIteration);
+                properties.store(out, null);
+            } catch (IOException e) { e.printStackTrace(); }
+        }));
+        
+        System.out.println("Creating backup directory if necessary...");
+        if(!Files.exists(destination))
+            Files.createDirectory(destination);
+        
+        System.out.printf("Resuming from last backup [%d]...\n", cIteration++);
+        while(true) {
+            if(cIteration >= mIteration)
+                cIteration = 0;
+            
+            String backup = prefix + "_" + String.format("%0" + ((int)Math.log10(mIteration) + 1) + "d.", cIteration) + extension;
+            String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+            Files.copy(source, destination.resolve(backup), StandardCopyOption.REPLACE_EXISTING);
+            System.out.printf("Backing up to %s at %s...\n", backup, timeStamp);
+            
+            Thread.sleep(TimeUnit.MINUTES.toMillis(delay));
+            cIteration++;
+        }
+    }
+}
